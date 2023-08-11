9e369adecb311b16b82605da5e1cb1b3085d6dad
analizcano <analucellylizcanoacero@gmail.com>
2023-08-11 11:12:57 -0500

    Clase 11 de agosto

---
+++ src/Main.java
@@ -0,0 +1,9 @@
+public class Main {
+    public static void main(String[] args) {
+
+        final int EDAD = 32;
+        //el final permite que no se pueda modificar la variable, es decir, la
+        //convierte en una constante
+        System.out.println("Age" + EDAD);
+    }
+}
---
+++ src/herencia/MainDomestico.java
@@ -0,0 +1,7 @@
+package herencia;
+
+public class MainDomestico {
+    public static void main(String[] args) {
+        Dog dog = new Dog("Coco",12);
+    }
+}
